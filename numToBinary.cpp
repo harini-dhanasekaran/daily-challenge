6b6ee5d1b8c2adba62893149c672f41361006651
Harini Dhanasekaran <63070913+harini-dhanasekaran@users.noreply.github.com>
2020-11-30 17:28:47 +0530

    added comments

--- numToBinary.cpp
+++ numToBinary.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 void bin(int n){
     for(int i=30;i>=0;i--){
-        if(n&(1<<i))
+        if(n&(1<<i))   //to avoid overflow we shift n ie (n<<i)&1 or use 1 along with 1ll 1ul
           cout<<1;
         else 
           cout<<0;
