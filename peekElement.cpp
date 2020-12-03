4cbf8caacc9eaecefc8eff2d168d96936aea1a1e
Harini Dhanasekaran <63070913+harini-dhanasekaran@users.noreply.github.com>
2020-12-03 21:13:12 +0530

    Easy

---
+++ peekElement.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int peakElement(int arr[], int n)
+{
+   if(arr[0]>arr[1])return 0;
+   if(arr[n-1]>arr[n-2])return n-1;
+   for(int i=1;i<=n-2;i++){
+       if(arr[i]>arr[i-1] && arr[i]>arr[i+1])
+         return i;
+   }
+}
+int main(){
+    int arr[4]={1,2,3,8};
+    cout<<peakElement(arr,4);
+    return 0;
+}
