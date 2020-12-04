97b7deae6997619fc0bdc095d6153802f7991c28
Harini Dhanasekaran <63070913+harini-dhanasekaran@users.noreply.github.com>
2020-12-04 17:52:32 +0530

    Easy(recursive)

---
+++ heightTree.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    Node *left;
+    int data;
+    Node *right;
+    Node(int x){data=x;left = NULL;right = NULL;}
+};
+Node *root=NULL;
+void insert(int data){
+    Node *t=root,*r,*c;
+    if(root==NULL){
+        c=new Node(data);
+        root=c;
+        return;
+    }
+    while(t!=NULL){
+        r=t;
+        if(data>t->data)
+          t=t->right;
+        else if(data<t->data)
+          t=t->left;
+        else
+          return;
+    }
+    c=new Node(data);
+    if(data>r->data)
+      r->right=c;
+    else if(data<r->data)
+      r->left=c;
+
+}
+int getHeight(Node* p){
+    int x,y;
+    if(p==NULL)
+      return 0;
+    x=getHeight(p->left);
+    y=getHeight(p->right);
+    return (x>y)?x+1:y+1;
+}
+int main(){
+    insert(10);
+    insert(5);
+    insert(20);
+    insert(2);
+    insert(15);
+    insert(7);
+    insert(25);
+    cout<<getHeight(root);
+    return 0;
+}
